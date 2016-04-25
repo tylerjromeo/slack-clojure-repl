fdb82187afc279d2c93fc9b80bca7bd86e453281
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-04-25 11:22:55 -0400

    respond to requests from slack by load-stringing the payload

--- src/slack_repl/handler.clj
+++ src/slack_repl/handler.clj
@@ -1,5 +1,6 @@
 (ns slack-repl.handler
-  (:require [compojure.core :refer :all]
+  (:require [clojure.string :refer [index-of]]
+            [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
@@ -7,8 +8,35 @@
   (assoc-in site-defaults
     [:security :anti-forgery] false))
 
+(defn strip-compile-error
+  "given a runtime exception, strips out the name of the exception"
+  [message]
+  (let [message-index (+ (clojure.string/index-of message \:) 2)]
+    (subs message message-index)))
+
+(defn run-command
+  "Evaluates a string representation of a clojure form. Returns the result or a sensible error messagesite-defaults"
+  [form]
+  (try
+    (str (load-string form))
+    ;TODO handle things that get println'd
+    ;TODO handle infinite loops
+    (catch Exception e (str "Uh-oh! " (strip-compile-error (.getMessage e))))))
+
 (defroutes app-routes
   (POST "/" [name] (str "Hello " name))
+  (POST "/slack-repl"
+    [token
+      team_id
+      team_domain
+      channel_id
+      channel_name
+      user_id
+      user_name
+      command
+      text ;this will contain the code to eval
+      response_url]
+    (run-command text))
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
