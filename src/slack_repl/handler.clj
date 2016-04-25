0efcd728cb77e4dfe5dfffc7b16d4127957e915b
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-04-25 10:22:24 -0400

    Turn off crsf token validation

--- src/slack_repl/handler.clj
+++ src/slack_repl/handler.clj
@@ -3,9 +3,13 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(def settings
+  (assoc-in site-defaults
+    [:security :anti-forgery] false))
+
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes settings))
